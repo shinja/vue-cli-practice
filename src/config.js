a4ff68a84cc46fbd1363f27f49333cd1e30803b8
finley <FinleyLu@trevi.cc>
2018-10-27 17:04:39 +0800

    after lint with initial config.
    ```
    npm run lint
    ```

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
-const xx = 1000;
+const xx = 1000
 const yy = {
-x: 100,
-        str: 'fdjslfsdjflsdfjsdflds lfjdslfdsjf slfdsjf ldsfjdsl ' + 'fjdsklfjdsflds fsjfldsfjdslfdsj dslfjdsflkdsfjd lkdfjdsf' +  ' ljfdsfkdsjfsdlkfdsjfsd lfjdsf ldsjfsld fjdsfl skf ',
+    x: 100,
+    str: 'fdjslfsdjflsdfjsdflds lfjdslfdsjf slfdsjf ldsfjdsl ' + 'fjdsklfjdsflds fsjfldsfjdslfdsj dslfjdsflkdsfjd lkdfjdsf' + ' ljfdsfkdsjfsdlkfdsjfsd lfjdsf ldsjfsld fjdsfl skf '
 }
 
 export default {
-xx,
-yy,
+    xx,
+    yy
 }
