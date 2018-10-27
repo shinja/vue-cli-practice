c93fe33df41aee0dc81b83c4f243639c5132b58d
finley <FinleyLu@trevi.cc>
2018-10-27 17:08:36 +0800

    Add eslint-config-prettier and .prettierrc configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
     root: true,
     env: {
-        node: true
+        node: true,
     },
-    'extends': [
+    extends: [
+        '@vue/standard',
         'plugin:vue/essential',
-        '@vue/standard'
+        'plugin:prettier/recommended',
     ],
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'indent': ['error', 4],
-        'import/no-webpack-loader-syntax': 'off'
+        indent: ['error', 4],
+        'import/no-webpack-loader-syntax': 'off',
     },
     parserOptions: {
-        parser: 'babel-eslint'
-    }
-}
+        parser: 'babel-eslint',
+    },
+};
