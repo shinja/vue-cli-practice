07e5d18794fcc89fcc263d164559d2fa71399e7c
finley <FinleyLu@trevi.cc>
2018-10-27 17:10:16 +0800

    prettify and lint after run
    ```
    npm run lint
    ```

--- src/App.vue
+++ src/App.vue
@@ -11,18 +11,18 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
     name: 'app',
     components: {
-        HelloWorld
-    }
+        HelloWorld,
+    },
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- src/config.js
+++ src/config.js
@@ -1,10 +1,13 @@
 const xx = 1000
 const yy = {
     x: 100,
-    str: 'fdjslfsdjflsdfjsdflds lfjdslfdsjf slfdsjf ldsfjdsl ' + 'fjdsklfjdsflds fsjfldsfjdslfdsj dslfjdsflkdsfjd lkdfjdsf' + ' ljfdsfkdsjfsdlkfdsjfsd lfjdsf ldsjfsld fjdsfl skf '
+    str:
+        'fdjslfsdjflsdfjsdflds lfjdslfdsjf slfdsjf ldsfjdsl ' +
+        'fjdsklfjdsflds fsjfldsfjdslfdsj dslfjdsflkdsfjd lkdfjdsf' +
+        ' ljfdsfkdsjfsdlkfdsjfsd lfjdsf ldsjfsld fjdsfl skf ',
 }
 
 export default {
     xx,
-    yy
+    yy,
 }
